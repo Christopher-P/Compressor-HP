5c0ef9dab6586229ee0ff6d875548db083a2d056
Christopher Pereyda <christopher.pereyda@gmail.com>
2019-10-15 16:50:10 -0700

    Init Commit

---
+++ main.py
@@ -0,0 +1,50 @@
+d = {}
+last = 'start'
+try:
+    with open("enwik8") as infile:
+        for line in infile:
+            for i in line:
+                if i in d.keys():
+                    #print(i, 'is in')
+                    #d[i] = []
+                    print(last, i)
+                    if last in d[i].keys():
+                        d[i][last] = d[i][last] + 1
+                    else:
+                        d[i][last] = {i:1}
+                else:
+                    d[last] = {i:1}
+                    
+                print(d)
+
+                last = i   
+except Exception as e:
+    print(e)
+    print(d)
+    print(len(d))
+
+
+'''
+        print(line)
+        words = line.split()
+        for i in words:
+            n = ''.join(x for x in i if x.isalpha())
+            if n in d.keys():
+                d[n] += 1
+            else:
+                d[n] = 0   
+                      
+                
+print(d)
+print(d['the'])
+print(d['from'])
+
+count = 0
+for i in d:
+    if d[i] > 4:
+        count += 1
+
+print(count)
+
+print(len(d))
+'''
