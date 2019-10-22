f33e63b3ee9b10c637240dea622e33753e4b0685
Christopher Pereyda <christopher.pereyda@gmail.com>
2019-10-21 22:44:01 -0700

    refactor
    Changed extension of text file to txt
    Added class for node code

--- C++/Compressor/Compressor/main.cpp
+++ C++/Compressor/Compressor/main.cpp
@@ -1,8 +1,43 @@
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+void compress();
+
+class Node {
+
+};
+
 
 int main() {
-	std::string input;
-	std::cout << "Hello World!" << std::endl;
-	std::cin >> input;
+	
+}
+
+void compress() {
+	string input;
+	cout << "Hello World!" << endl;
+
+
+	ifstream infile("small-data.txt");
+
+
+	string line;
+	while (getline(infile, line))
+	{
+		ifstream file("small-data.txt");
+		if (file.is_open()) {
+			string line;
+			while (getline(file, line)) {
+				// using printf() in all tests for consistency
+				printf("%s", line.c_str());
+			}
+			file.close();
+		}
+	}
+
+	cin >> input;
 }
